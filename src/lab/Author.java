81a17ef275913ab3b60891cf651d0c492de57f2b
ExxxxT <aleks9308@gmail.com>
2020-09-15 00:37:29 +0300

    1

---
+++ src/lab/Author.java
@@ -0,0 +1,15 @@
+package lab;
+
+class Author {
+
+        public static void main(String[] args)
+        {
+            AuthorTest author1 = new AuthorTest("Alex", "aleks9308@gmail.com", 'M');
+            AuthorTest author2 = new AuthorTest("Jenya", "abdc@gmail.com", 'F');
+
+            System.out.println(author1);
+            System.out.println();
+            System.out.println(author2);
+        }
+    }
+
